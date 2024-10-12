387b0b742b097a7fc0ada88c9ad75961438822fc
Patter1989 <125509804+Patter1989@users.noreply.github.com>
2024-10-12 21:10:32 +0200

    2

--- src/server.js
+++ src/server.js
@@ -16,19 +16,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
 
-  app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
-    }),
-  );
-
-  // app.use('*', (req, res, next) => {
-  //   res.status(404).json({
-  //     message: 'Not found',
-  //   });
-  // });
+  
 
   app.use((err, req, res, next) => {
     res.status(500).json({
@@ -63,4 +51,16 @@ export const setupServer = () => {
       data: contact,
     });
   });
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
 };
